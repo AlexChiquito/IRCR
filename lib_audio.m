a0dd44da1bfa629f2006d92a5d52a08e89b8951f
AlexChiquito <alejandrochi_257@hotmail.com>
2015-04-29 14:29:34 -0500

    dsadsadsaasdasdasd

--- lib_audio.m
+++ lib_audio.m
@@ -3,7 +3,9 @@ nargin
 global fs n;
 
 if nargin > 0
-    lib_audio=wavread(file);
+    % lib_audio=wavread(file);
+    %lib_audio=audioread(file);
+    %lib_audio=lib_audio(1,:);
 else
     lib_audio=wavrecord(n*fs,fs);
 end
